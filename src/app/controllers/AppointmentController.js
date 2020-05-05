779d00ec4e9afe09467c054725a6ad6bb7b6fb85
Fábio Alexandre Mendes Camillo <fabio.camillo@asteria.com.br>
2020-05-04 21:11:40 -0300

    Verifyung dates with date-fns

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
-import * as Yup from 'yup';
 
 class AppointmentController {
     async store(req, res) {
@@ -26,13 +27,33 @@ class AppointmentController {
         if (!isProvider) {
             return res
                 .status(401)
-                .json({ error: 'You can only create appointments with providers ' });
+                .json({ error: 'You can only create appointments with providers' });
+        }
+
+        /**
+         * Check for past dates
+         */
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Past dates are not permitted' });
+        }
+
+        /**
+         * Check date availabity
+         */
+        const checkAvailabity = await Appointment.findOne({
+            where: { provider_id, canceled_at: null, date: hourStart }
+        })
+
+        if (checkAvailabity) {
+            return res.status(400).json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date
+            date,
         });
 
         return res.json(appointment);
